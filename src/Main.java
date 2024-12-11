af211eb187e61f6a819989a376fda230299fbb18
Dhruv <guptadhruv1908@gmail.com>
2024-12-11 14:34:12 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to Employee Wage Computation");
+        System.out.println("Program on Main Branch");
+    }
+}
