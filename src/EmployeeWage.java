1fe50a9a580af63a21a6f6d799884275a4b82f91
Dhruv <guptadhruv1908@gmail.com>
2024-12-11 14:46:09 +0530

    Wage Calculation

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -7,11 +7,17 @@ public class EmployeeWage {
 
         System.out.println("Welcome to Employee Wage Computation");
         double attendanceCheck = Math.floor(Math.random() * 10) % 2;
-        if (attendanceCheck == 1) {
-            System.out.println("Employee is Present");
+        int WagePerHr = 20;
+        int EmpWage;
+        if(attendanceCheck == 1){
+            System.out.println("Employee is PRESENT");
+            System.out.println("Employee's Wage: " + 8*WagePerHr);
         }
         else {
-            System.out.println("Employee is Absent");
+            System.out.println("Employee is ABSENT");
+            System.out.println("Employee's Wage: " + 0*WagePerHr);
         }
+
+
     }
 }
