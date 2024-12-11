febc9768e299297b543a84f4fd9fdb09a51bfc46
Dhruv <guptadhruv1908@gmail.com>
2024-12-11 14:58:37 +0530

    Condition

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -5,25 +5,40 @@ public class EmployeeWage {
         int WagePerHr = 20;
         int FullDayHrs = 8;
         int PartTimeHrs = 4;
-        int WorkingDays = 20;
+        int MaxWorkingHrs = 100;
+        int MaxWorkingDays = 20;
+
+        int totalHrs = 0;
+        int totalDays = 0;
         int totalWage = 0;
 
-        System.out.println("Calculating Wages for a Month (20 Working Days)");
-        for (int day = 1; day <= WorkingDays; day++) {
+
+
+        System.out.println("Calculating Wages till Total Working Hours or Days is Reached");
+
+        while (totalHrs < MaxWorkingHrs && totalDays < MaxWorkingDays) {
             int attendance = (int) Math.floor(Math.random() * 10) % 3;
+            totalDays++;
 
             switch (attendance) {
                 case 1:
-                    totalWage += FullDayHrs * WagePerHr;
+                    if (totalHrs + FullDayHrs <= MaxWorkingHrs) {
+                        totalHrs += FullDayHrs;
+                        totalWage += FullDayHrs * WagePerHr;
+                    }
                     break;
                 case 2:
-                    totalWage += PartTimeHrs * WagePerHr;
+                    if (totalHrs + PartTimeHrs <= MaxWorkingHrs) {
+                        totalHrs += PartTimeHrs;
+                        totalWage += PartTimeHrs * WagePerHr;
+                    }
                     break;
                 default:
                     break;
             }
         }
-
-        System.out.println("Total Wage for the Month: " + totalWage);
+        System.out.println("Total Working Days: " + totalDays);
+        System.out.println("Total Working Hours: " + totalHrs);
+        System.out.println("Total Wage: " + totalWage);
     }
 }
