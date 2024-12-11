5346b1a6399372c18fee733b82f8a8163069c8c1
Dhruv <guptadhruv1908@gmail.com>
2024-12-11 23:25:09 +0530

    uc7 day4 commit

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,25 +1,23 @@
-import java.util.Random;
-
 public class EmployeeWage {
-    public static void main(String[] args) {
-        int WagePerHr = 20;
-        int FullDayHrs = 8;
-        int PartTimeHrs = 4;
-        int MaxWorkingHrs = 100;
-        int MaxWorkingDays = 20;
 
+    static int WagePerHr = 20;
+    static int FullDayHrs = 8;
+    static int PartTimeHrs = 4;
+    static int MaxWorkingHrs = 100;
+    static int MaxWorkingDays = 20;
+    int totalHrs = 0;
+    int totalDays = 0;
+    int totalWage = 0;
+
+    public static int calculateWage() {
         int totalHrs = 0;
         int totalDays = 0;
         int totalWage = 0;
 
-
-
         System.out.println("Calculating Wages till Total Working Hours or Days is Reached");
-
         while (totalHrs < MaxWorkingHrs && totalDays < MaxWorkingDays) {
             int attendance = (int) Math.floor(Math.random() * 10) % 3;
             totalDays++;
-
             switch (attendance) {
                 case 1:
                     if (totalHrs + FullDayHrs <= MaxWorkingHrs) {
@@ -37,8 +35,15 @@ public class EmployeeWage {
                     break;
             }
         }
+
         System.out.println("Total Working Days: " + totalDays);
         System.out.println("Total Working Hours: " + totalHrs);
         System.out.println("Total Wage: " + totalWage);
+
+        return totalWage;
+    }
+
+    public static void main(String[] args) {
+        calculateWage();
     }
 }
